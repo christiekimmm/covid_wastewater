5b78b43c53528956c863a599732743408b756100
Christie Kim <upd8@cdc.gov>
2024-07-23 11:18:33 -0400

    Updated the angle of x-axis labels

--- source/04_data_visualization.R
+++ source/04_data_visualization.R
@@ -3,14 +3,14 @@ load(here::here("data", "clean.Rdata"))
 library(tidyverse)
 library(forcats)
 
-
+# add a comment!
 plot1 = covid %>%
   mutate(county = factor(county),
          county = fct_reorder(county, concentration)) %>%
   ggplot(aes(county, log(concentration), group = county, fill = county)) +
   geom_boxplot() +
   theme(legend.position = "none",
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 30, hjust = 1))
 
 
 ## gonna do something here with the dates to make this prettier.
